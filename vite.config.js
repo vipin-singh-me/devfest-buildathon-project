d063d5d8b3db1c31b5d2b1165b147270252a3b1c
Vipin Singh <vipinsingh.sirte@gmail.com>
2025-11-09 07:59:52 +0530

    Added more for deploying

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'index.html',
+          dest: '.', // create 404.html in dist
+          rename: '404.html'
+        }
+      ]
+    })
+  ],
   base: "/devfest-buildathon-project/",
 })
